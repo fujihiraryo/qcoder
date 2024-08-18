0c34c9034485c3099d602553b4ea6ea58d642a1f
fujihiraryo <fujihiraryo@team-lab.com>
2024-08-18 09:33:49 +0900

    test code

--- QPC001/B1.py
+++ QPC001/B1.py
@@ -1,15 +1,18 @@
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
 
 
 def solve() -> QuantumCircuit:
     qc = QuantumCircuit(2)
-    # qc.cx(0, 1)
-    qc.initialize("01", qc.qubits)
+    qc.cx(0, 1)
     return qc
 
 
 from qiskit.quantum_info import Statevector
 
 qc = solve()
+test = QuantumCircuit(2)
+test.initialize("11")
+test = test.compose(qc)
 print(qc.draw())
-print(Statevector(qc).data)
+print(test.draw())
+print(Statevector(test).data)
